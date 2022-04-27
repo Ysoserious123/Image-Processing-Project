44cd8179ddfb3c57a50af41250b59282ab895744
Ysoserious123 <Ysoserious124@gmail.com>
2022-04-27 19:40:16 -0400

    Fixed modified level set
    I also added new functions to PhotoProcessing to easily run different photo processing methods.

--- PhotoProcessing.py
+++ PhotoProcessing.py
@@ -28,12 +28,48 @@ def levelSet(currentState, h, dt, epsilon):
 
     return next
 
-def modifiedLevelSet(currentState, original, h, dt, alpha, epsilon):
+def modifiedLevelSet(currentState, original, h, dt, epsilon, alpha):
     prev = np.copy(currentState)
-
     ux, uy, uxy, uxx, uyy = diff.levelSetPartials(prev, h, h)
-    prev[1:-1, 1:-1] = prev[1:-1, 1:-1] + (dt/(h**2))*((((ux**2)*uyy) - (2*ux*uy*uxy) + ((uy**2)*uxx)) / (ux**2 + uy**2 + epsilon))[1:-1, 1:-1] - alpha*(prev[1:-1, 1:-1] - original[1:-1, 1:-1])
+    prev[1:-1, 1:-1] = prev[1:-1, 1:-1] + (dt/(h**2))*((((ux**2)*uyy) - (2*ux*uy*uxy) + ((uy**2)*uxx)) / (ux**2 + uy**2 + epsilon))[1:-1, 1:-1]
+
+    prev[1:-1, 1:-1] = prev[1:-1, 1:-1] - alpha*(prev[1:-1, 1:-1] - original[1:-1, 1:-1])
 
     next = prev
 
     return next
+
+def runHeatEquation(image, total_time, dt):
+    time = 0.0
+    initial_image = np.copy(image)
+    while time < total_time:
+        image = heatEquation(image, 1, dt)
+        time += dt
+    return (initial_image, image)
+
+def runLevelSet(image, total_time, dt, epsilon):
+    time = 0.0
+    initial_image = np.copy(image)
+    while time < total_time:
+        image = levelSet(image, 1, dt, epsilon)
+        time += dt
+    return (initial_image, image)
+
+def runModifiedLevelSet(image, total_time, dt, epsilon, alpha):
+    # print("running modified level set", epsilon, alpha)
+    time = 0.0
+    initial_image = np.copy(image)
+    while time < total_time:
+        image = modifiedLevelSet(image, initial_image, 1, dt, epsilon, alpha)
+        time += dt
+    return (initial_image, image)
+
+def processImage(image, method, total_time, dt, **kwargs):
+
+    method_dict = {'heat': lambda : runHeatEquation(image, total_time, dt), 
+               'level-set': lambda : runLevelSet(image, total_time, dt, kwargs[0]), 
+               'modified-level-set': lambda : runModifiedLevelSet(image, total_time, dt, kwargs['epsilon'], kwargs['alpha'])
+               }
+
+    return method_dict[method]()
+    
