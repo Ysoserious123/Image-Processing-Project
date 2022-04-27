Fixed errors in the mixed partial derivative func

DifferentialUtility.py
@@ -97,7 +97,7 @@ def mixedPartial(prev, dx, dy):
     dudxdy = np.copy(prev)
     for i in range(1, len(prev) - 1):
         for j in range(1, len(prev[i]) - 1):
-            dudxdy[i, j] = (prev[i+1, j+1] - prev[i-1, j+1] - prev[i+1, j-1] - prev[i-1, j-1])/(4*dx*dy)
+            dudxdy[i, j] = (prev[i+1, j+1] - prev[i-1, j+1] - prev[i+1, j-1] + prev[i-1, j-1])/(4*dx*dy)
     
     return dudxdy
 
@@ -144,7 +144,7 @@ def levelSetPartials(prev, dx, dy):
 
     for i in range(1, len(prev) - 1):
         for j in range(1, len(prev[i]) - 1):
-            uxy[i, j] = (prev[i+1, j+1] - prev[i-1, j+1] - prev[i+1, j-1] - prev[i-1, j-1])/(4*dx*dy)
+            uxy[i, j] = (prev[i+1, j+1] - prev[i-1, j+1] - prev[i+1, j-1] + prev[i-1, j-1])/(4*dx*dy)
             uy[i, j] = (prev[i, j+1] - prev[i, j-1])/(2*dy)
             ux[i, j] = (prev[i+1, j] - prev[i-1, j])/(2*dx)
             uyy[i, j] = (prev[i, j+1] - (2*prev[i, j]) + prev[i, j-1])/(dy**2)

Question1.py
@@ -10,7 +10,7 @@ import numpy as np
 import PhotoProcessing as pp
 
 
-im = imread("images/happyfacenoisemedium.jpg") # Load an image
+im = imread("images/shapes.jpg") # Load an image
 im = im/255 # Normalize the image (black and white scale with the largest value being 255)
 # print(im) # prints the image object (which is stored as a 2d numpy.ndarray) to the console
 
@@ -28,12 +28,12 @@ total_time = 0.0
 
 def animate(frames):
     global im, total_time
-    dt = 0.1
+    dt = 0.5
 
     original = np.copy(im)
 
-    im = pp.modifiedLevelSet(im, original, 1, dt, 10000000, 0.000001)
-    # im = pp.levelSet(im, 1, dt, 0.000001)
+    # im = pp.modifiedLevelSet(im, original, 1, dt, 0.5, 0.000001)
+    im = pp.levelSet(im, 1, dt, 0.000001)
     # im = pp.heatEquation(im, 1, dt)
 
     total_time += dt